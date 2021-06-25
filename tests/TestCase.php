977a6a1733f1803246bb4eba239f50a3ee1ab71f
Marius Claassen <marius.claassen@outlook.com>
2021-06-25 19:46:54 +0200

    Remove call to set config values to change database used

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,17 +25,8 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function defineEnvironment($app)
     {
-
-        $app->config->set('database.default', 'mysql');
-        $app->config->set('database.connections.msql.database', 'forge');
-        $app->config->set('database.connections.mysql.username', 'root');
-        $app->config->set('database.connections.mysql.password', 'Ming2013');
-
-        Schema::dropIfExists('quotes');
-        // import the CreateQuotesTable class from our migration
         include_once __DIR__ . '/../database/migrations/create_quotes_table.php.stub';
 
-        // run the up method of the migration
         (new \CreateQuotesTable())->up();
     }
 }
