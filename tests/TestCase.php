f7a8ee394511399a027eac8dfe2224e4dbd44e17
Marius Claassen <marius.claassen@outlook.com>
2021-06-25 12:36:34 +0200

    Add define Environment method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,13 @@
 namespace Claassenmarius\LaravelSkynet\Tests;
 
 use Claassenmarius\LaravelSkynet\SkynetServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use RefreshDatabase;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -19,8 +23,19 @@ class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
-    protected function getEnvironmentSetUp($app)
+    protected function defineEnvironment($app)
     {
-        // perform environment setup
+
+        $app->config->set('database.default', 'mysql');
+        $app->config->set('database.connections.msql.database', 'forge');
+        $app->config->set('database.connections.mysql.username', 'root');
+        $app->config->set('database.connections.mysql.password', 'Ming2013');
+
+        Schema::dropIfExists('quotes');
+        // import the CreateQuotesTable class from our migration
+        include_once __DIR__ . '/../database/migrations/create_quotes_table.php.stub';
+
+        // run the up method of the migration
+        (new \CreateQuotesTable())->up();
     }
 }
