38480846b55f633c1b1f141da6ceb8ea2a07194f
Marius Claassen <marius.claassen@outlook.com>
2021-06-25 19:43:25 +0200

    Remove call to set config values to change database used

--- config/config.php
+++ config/config.php
@@ -11,10 +11,10 @@ return [
     | account number.
     */
 
-    'account_username' => env('SKYNET_ACCOUNT_USERNAME'),
-    'account_password' => env('SKYNET_ACCOUNT_PASSWORD'),
-    'system_id' => env('SKYNET_SYSTEM_ID'),
-    'account_number' => env('SKYNET_ACCOUNT_NUMBER'),
+    'account_username' => env('SKYNET_ACCOUNT_USERNAME', 'iDeliverTest'),
+    'account_password' => env('SKYNET_ACCOUNT_PASSWORD', '!D3liver1!'),
+    'system_id' => env('SKYNET_SYSTEM_ID', '2'),
+    'account_number' => env('SKYNET_ACCOUNT_NUMBER', 'J99133'),
 
     'prefix' => 'quote',
     'middleware' => ['web'],
