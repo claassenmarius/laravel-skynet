df9405d59f683b0878d724be9c6a0c782f3bf2c4
Marius Claassen <marius.claassen@outlook.com>
2021-06-25 12:33:33 +0200

    Add route prefix and middleware

--- config/config.php
+++ config/config.php
@@ -1,8 +1,21 @@
 <?php
 
 return [
-    'account_username' => env('SKYNET_ACCOUNT_USERNAME', 'iDeliverTest'),
-    'account_password' => env('SKYNET_ACCOUNT_PASSWORD', '!D3liver1!'),
-    'system_id' => env('SKYNET_SYSTEM_ID', 2),
-    'account_number' => env('SKYNET_ACCOUNT_NUMBER', 'J99133'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Skynet Account Credentials
+    |--------------------------------------------------------------------------
+    |
+    | Here you can set your skynet account username, password, system id and
+    | account number.
+    */
+
+    'account_username' => env('SKYNET_ACCOUNT_USERNAME'),
+    'account_password' => env('SKYNET_ACCOUNT_PASSWORD'),
+    'system_id' => env('SKYNET_SYSTEM_ID'),
+    'account_number' => env('SKYNET_ACCOUNT_NUMBER'),
+
+    'prefix' => 'quote',
+    'middleware' => ['web'],
 ];
